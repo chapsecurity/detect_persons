9bf6e3cbc244d9b34c56e3d9aa94b56c929a783b
hussein <hussein.alolimat@gmail.com>
2024-11-18 23:25:28 -0600

    fix bug in g assignment

--- app.py
+++ app.py
@@ -15,20 +15,20 @@ app = Flask(__name__)
 
 # Model paths defined using environment variables for flexibility
 MODEL_PATHS = {
-    "model_n": "models/yolo11n.pt",
-    "faces_model": "models/yolov8n-face.pt",
+    "model_n": os.getenv("MODEL_N_PATH", "dev/yolo11n.pt"),
+    "faces_model": os.getenv("FACE_MODEL_PATH", "yolov8n-face.pt"),
 }
 
 
 def get_model(model_name):
     """Load and return the YOLO model from Flask's application context."""
-    if model_name not in g:
+    if not hasattr(g, model_name):
         if model_name in MODEL_PATHS:
             logger.info(f"Loading {model_name} model")
-            g[model_name] = YOLO(MODEL_PATHS[model_name], verbose=False)
+            setattr(g, model_name, YOLO(MODEL_PATHS[model_name], verbose=False))
         else:
             raise ValueError(f"Model {model_name} not supported.")
-    return g[model_name]
+    return getattr(g, model_name)
 
 
 def load_image(input_data):
@@ -55,7 +55,7 @@ def crop_padded_box(image, box, padding, image_width, image_height):
     return cv2.cvtColor(cropped_object, cv2.COLOR_BGR2RGB)
 
 
-@app.route("/detect_people", methods=["POST"])
+@app.route("/detect_persons", methods=["POST"])
 def detect_persons_route():
     """Flask route to detect persons in an image."""
     if "image" not in request.files:
@@ -146,9 +146,8 @@ def detect_faces_route():
 def teardown(exception):
     """Clean up models from Flask's application context after request."""
     for model_name in MODEL_PATHS.keys():
-        model = g.pop(model_name, None)
-        if model is not None:
-            del model
+        if hasattr(g, model_name):
+            delattr(g, model_name)
 
 
 if __name__ == "__main__":
